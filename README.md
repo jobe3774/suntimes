48614e65fc6c3cbf1a2409b27d2e96f0aaf54855
jobe <jobe@jobe-software.de>
2019-10-21 22:23:43 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# suntimes
+A [raspend](https://github.com/jobe3774/raspend) based application serving the times for sunrise, sun's culmination and sunset.
+
+The calculation is based on SunMoon.py by Michael Dalder, which in turn is a port of Arnold Barmettler's JavaScript. You can find both [here](https://lexikon.astronomie.info/java/sunmoon/).
---
+++ suntimes.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  Calculate the times for sunrise, sun's culmination and sundown for yesterday, today and tomorrow 
+#  for given geographical coordinates.
+#
+#  The calculation is based on SunMoon.py by Michael Dalder, which in turn is a port of Arnold Barmettler's JavaScript. 
+#  You can find both on https://lexikon.astronomie.info/java/sunmoon/.
+#  
+#  License: MIT
+#  
+#  Copyright (c) 2019 Joerg Beckers
+
+import argparse
+
+from datetime import datetime, timedelta
+
+from raspend.application import RaspendApplication
+from raspend.utils import dataacquisition as DataAcquisition
+
+from SunMoon import SunMoon
+
+class SunTimes():
+    def calc(self, dateStr, longitude: float, latitude: float):
+        try:
+            calcDate = datetime.strptime(dateStr, "%Y-%m-%d")
+        except ValueError as e:
+            return e
+
+        sunMoon = SunMoon(float(longitude), float(latitude), calcDate)
+        sunRiseSet = sunMoon.GetSunRiseSet()
+
+        sunTimesDict = { "SunTimes" : {"Sunrise": "--:--", "Culmination" : "--:--", "Sundown": "--:--", "Date": ""} }
+
+        sunTimesDict["SunTimes"]["Date"] = dateStr
+        sunTimesDict["SunTimes"]["Sunrise"] = sunRiseSet[0]
+        sunTimesDict["SunTimes"]["Culmination"] = sunRiseSet[1]
+        sunTimesDict["SunTimes"]["Sundown"] = sunRiseSet[2]
+
+        return sunTimesDict
+
+def main():
+
+    # Check commandline arguments.
+    cmdLineParser = argparse.ArgumentParser(prog="suntimes", usage="%(prog)s [options]")
+    cmdLineParser.add_argument("--port", help="The port the server should listen on.", type=int, required=True)
+
+    try: 
+        args = cmdLineParser.parse_args()
+    except SystemExit:
+        return
+
+    sunTimesApp = RaspendApplication(args.port)
+
+    sunTimesApp.addCommand(SunTimes().calc)
+
+    sunTimesApp.run()
+
+if __name__ == "__main__":
+    main()
